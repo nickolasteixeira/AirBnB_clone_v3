2864789add5c37ebc316fd514626816c5723e81c
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-08-16 15:19:24 +0000

    stats on objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response
+from models import storage
+from models import Amenity
+from models import City
+from models import Place
+from models import Review
+from models import State
+from models import User
 
 
 @app_views.route('/status')
 def return_status():
+    ''' status '''
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def return_stats():
+    ''' stats '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
