b20fdebf183f446525d8f59317df61badabe2de4
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-08-16 14:33:43 +0000

    include self for teardown, change sequent of import for app, usr/bin/python3

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,17 +11,19 @@ Basic Flask App
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown():
+def teardown(self):
     """
     Closes session
     """
     storage.close()
 
+
 if __name__ == "__main__":
-    env_var = {'host': '0.0.0.0', 'port':'5000'}
-    if getenv(HBNB_API_HOST):
-        env_var[host] = getenv(HBNB_API_HOST)
-    if getenv(HBNB_API_PORT):
-        env_var[port] = getenv(HBNB_API_PORT)
-    app.run(host=env_var[host], port=env_var[port], threaded=True)
+    env_var = {'host': '0.0.0.0', 'port': 5000}
+    if getenv('HBNB_API_HOST'):
+        env_var['host'] = getenv('HBNB_API_HOST')
+    if getenv('HBNB_API_PORT'):
+        env_var['port'] = getenv('HBNB_API_PORT')
+    app.run(host=env_var.get('host'), port=env_var.get('port'), threaded=True)
