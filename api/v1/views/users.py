0e059f661f5d17c73f3826c5ad4bb3f0c720d904
Andrew Keil <monkeyninja147@gmail.com>
2018-08-22 19:02:06 +0000

    Adding Get all method to users 2.0

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,6 +6,13 @@ from models import storage
 from models import User
 
 
+@app_views.route('/users/', methods=['GET'], strict_slashes=False)
+def get_users():
+    ''' returns all users '''
+    users = [user.to_dict() for user in storage.all('User').values()]
+    return jsonify(users)
+
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def return_user(user_id):
     '''return user corresponding to user_id'''
